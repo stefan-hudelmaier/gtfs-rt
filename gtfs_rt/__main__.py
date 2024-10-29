1a58c5fec8584d1a398d119bff0e64aafcd08f46
Stefan Hudelmaier <stefan.hudelmaier@device-insight.com>
2024-10-29 22:44:31 +0100

    Generate readmes

--- gtfs_rt/__main__.py
+++ gtfs_rt/__main__.py
@@ -1,5 +1,8 @@
 import os
 import json
+from pprint import pformat
+from venv import create
+
 from google.transit import gtfs_realtime_pb2
 import requests
 from dataclasses import dataclass
@@ -7,12 +10,14 @@ from dotenv import load_dotenv
 import logging
 import sys
 import paho.mqtt.client as mqtt
+from paho.mqtt.packettypes import PacketTypes
+from paho.mqtt.properties import Properties
 
 load_dotenv()
 
-feed = gtfs_realtime_pb2.FeedMessage()
-
 TRANSITLAND_FEED_DIR = os.environ["TRANSITLAND_FEED_DIR"]
+
+generate_gcmb_readme = os.environ.get("GENERATE_GCMB_README", "false") == "true"
 
 last_modified_cache = {}
 
@@ -36,11 +41,49 @@ logger.addHandler(handler)
 
 
 @dataclass
+class License:
+    spdx_identifier: str
+    use_without_attribution: bool
+    create_derived_product: bool
+    commercial_use_allowed: bool
+    redistribution_allowed: bool
+    url: str
+
+    def can_be_used(self):
+        if self.spdx_identifier in ['CC-BY-4.0', 'ODbL-1.0', 'ODC-By-1.0']:
+            return True
+
+        if self.use_without_attribution:
+            return True
+
+        if self.create_derived_product:
+            return True
+
+        if self.commercial_use_allowed:
+            return True
+
+        if self.redistribution_allowed:
+            return True
+
+
+
+@dataclass
 class Feed:
     operator_name: str
     url: str
     authorization: dict
+    license: License
 
+
+def create_license(license_obj):
+    return License(
+        spdx_identifier=license_obj.get('spdx_identifier'),
+        use_without_attribution=license_obj.get('use_without_attribution') == 'yes',
+        create_derived_product=license_obj.get('create_derived_product') == 'yes',
+        commercial_use_allowed=license_obj.get('commercial_use_allowed') == 'yes',
+        redistribution_allowed=license_obj.get('redistribution_allowed') == 'yes',
+        url=license_obj.get('url')
+    )
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc, properties):
@@ -60,32 +103,8 @@ def connect_mqtt():
     return mqtt_client
 
 
-def can_be_used(license_obj):
-    if license_obj.get('spdx_identifier') in ['CC-BY-4.0', 'ODbL-1.0', 'ODC-By-1.0']:
-        return True
 
-    if license_obj.get('use_without_attribution') == 'yes':
-        return True
-
-    if license_obj.get('create_derived_product') == 'yes':
-        return True
-
-    if license_obj.get('commercial_use_allowed') == 'yes':
-        return True
-
-    if license_obj.get('redistribution_allowed') == 'yes':
-        return True
-
-
-def get_rt_feeds_from_file_content(filename, data):
-
-    # pprint.pp(data)
-    feeds = []
-
-    associated_operators = {}
-
-    operators = data.get('operators') or []
-
+def populate_associated_operators(operators, associated_operators):
     for operator in operators:
         associated_feeds = operator.get('associated_feeds') or []
         for associated_feed in associated_feeds:
@@ -94,17 +113,22 @@ def get_rt_feeds_from_file_content(filename, data):
                 continue
             associated_operators[associated_feed_id] = operator
 
+
+
+def get_rt_feeds_from_file_content(filename, data):
+
+    feeds = []
+
+    associated_operators = {}
+
+    operators = data.get('operators') or []
+    populate_associated_operators(operators, associated_operators)
+
     for feed in data['feeds']:
         spec = feed['spec']
 
         feed_operators = feed.get('operators') or []
-        for operator in feed_operators:
-            associated_feeds = operator.get('associated_feeds') or []
-            for associated_feed in associated_feeds:
-                ssociated_feed_id = associated_feed.get('feed_onestop_id')
-                if associated_feed_id is None:
-                    continue
-                associated_operators[associated_feed_id] = operator
+        populate_associated_operators(feed_operators, associated_operators)
 
         if spec == 'gtfs-rt':
             urls = feed['urls']
@@ -113,18 +137,18 @@ def get_rt_feeds_from_file_content(filename, data):
             if not 'license' in feed:
                 continue
 
-            license_obj = feed['license']
-            if not can_be_used(license_obj):
-                print("Cannot use", filename, "for feed", feed['id'], "due to license:", license_obj)
+            feed_license = create_license(feed['license'])
+            if not feed_license.can_be_used():
+                logger.info(f"Cannot use {filename} for feed {feed['id']} due to license: {feed['license']}")
                 continue
 
             operator = feed.get('operator') or associated_operators.get(feed['id'])
             if operator is None:
-                print("No operator", filename, "for feed", feed['id'])
-         mqtt_client = connect_mqtt()       continue
+                logger.info(f"No operator {filename} for feed {feed['id']}")
+                continue
 
             authorization = feed.get('authorization')
-            feeds.append(Feed(operator['name'], urls['realtime_vehicle_positions'], authorization))
+            feeds.append(Feed(operator['name'], urls['realtime_vehicle_positions'], authorization, feed_license))
 
     return feeds
 
@@ -150,27 +174,32 @@ def get_rf_feeds():
     return all_feeds
 
 
-def get_vehicle_positions(url):
+def get_gtfs_rt(url):
 
     response = requests.get(url, headers={'If-Modified-Since': last_modified_cache.get(url)})
 
     if response.status_code == 304:
-        print("Not modified", url)
-        return []
+        logger.debug("Not modified {url}")
+        return None
 
     if not response.status_code == 200:
-        return []
+        return None
 
     last_modified = response.headers.get('Last-Modified')
     if last_modified:
         last_modified_cache[url] = last_modified
 
-    feed.ParseFromString(response.content)
+    return response.content
+
+
+def get_vehicle_positions(gtfs_rt_message_pb):
+
+    gtfs_rt_message = gtfs_realtime_pb2.FeedMessage()
+    gtfs_rt_message.ParseFromString(gtfs_rt_message_pb)
+
     vehicle_positions = []
-    for entity in feed.entity:
-        # print(entity)
+    for entity in gtfs_rt_message.entity:
         if entity.HasField('vehicle'):
-            print(entity.vehicle)
             vehicle_positions.append(entity.vehicle)
 
     return vehicle_positions
@@ -179,43 +208,92 @@ def get_vehicle_positions(url):
 def operator_name_to_topic(operator_name):
     return operator_name.replace(" ", "_").replace("/", "_").replace("-", "_").replace("(", "").replace(")", "").replace( ",", "").replace(".", "").replace(":", "").replace(";", "").replace("'", "").replace("’", "")
 
+
+def fetch_from_feed(feed: Feed, mqtt_client: mqtt.Client):
+
+    gtfs_rt_message_pb = get_gtfs_rt(feed.url)
+
+    if gtfs_rt_message_pb is None:
+        return
+
+    operator_topic_part = operator_name_to_topic(feed.operator_name)
+
+    gtfs_pb_topic = f"stefan/public-transport/{operator_topic_part}/pb"
+    logger.debug(f"Publishing to {gtfs_pb_topic}")
+
+    properties = Properties(PacketTypes.PUBLISH)
+    properties.UserProperty = ("Content-Type", "application/protobuf")
+
+    # Publish protobuf gtfs-rt message representing the complete realtime state of the feed
+    result = mqtt_client.publish(
+        gtfs_pb_topic,
+        gtfs_rt_message_pb,
+        retain=True,
+        properties=properties
+    )
+    logger.debug(f"Publish result: {result}")
+
+    vehicle_positions = get_vehicle_positions(gtfs_rt_message_pb)
+
+    # Publish individual vehicle positions
+    for vehicle_position in vehicle_positions:
+        position_topic = f"stefan/public-transport/{operator_topic_part}/vehicle_positions/{vehicle_position.vehicle.id}"
+        logger.debug(f"Publishing to {position_topic}")
+        result = mqtt_client.publish(position_topic,
+                            f"{vehicle_position.position.latitude},{vehicle_position.position.longitude}", retain=True)
+        logger.debug(f"Publish result: {result}")
+
+
+def generate_gcmb_readme(feed: Feed):
+    return f"""# {feed.operator_name}
+    
+## License
+
+Name: {feed.license.spdx_identifier}
+URL: {feed.license.url}
+
+Redistribution allowed: {feed.license.redistribution_allowed}
+Commercial use allowed: {feed.license.commercial_use_allowed}
+Create derived product: {feed.license.create_derived_product}
+Use without attribution: {feed.license.use_without_attribution}
+
+## Map
+
+<WorldMap topic="stefan/public-transport/{operator_name_to_topic(feed.operator_name)}/vehicle_positions/#" />
+"""
+
+
+def generate_gcmb_readmes(feeds: list[Feed]):
+    if generate_gcmb_readme:
+        for feed in feeds:
+            readme = generate_gcmb_readme(feed)
+            operator_name = operator_name_to_topic(feed.operator_name)
+            folder = f"generated/gcmb/{operator_name}"
+            os.makedirs(folder, exist_ok=True)
+            with open(f"{folder}/README.md", "w") as f:
+                f.write(readme)
+
+
 def main():
 
-    rt_feeds = get_rf_feeds()
+    mqtt_client = connect_mqtt()
+    mqtt_client.loop_start()
 
-    # rt_feeds = get_rt_feeds_from_file("exo.quebec.dmfr.json")
-    # pprint.pp(rt_feeds)
+    rt_feeds = get_rf_feeds()
+    logger.debug(f"Using the following feeds: {pformat(rt_feeds)}")
 
     rt_feeds_no_auth = [f for f in rt_feeds if f.authorization is None]
-    
-    print("Total feeds:", len(rt_feeds))
-    print("Feeds without authorization:", len(rt_feeds_no_auth))
 
-    # Germany, but does not have vehicle positions
-    # url = 'https://realtime.gtfs.de/realtime-free.pb'
+    generate_gcmb_readmes(rt_feeds_no_auth)
+
+    logger.info(f"Total feeds: {len(rt_feeds)}")
+    logger.info(f"Feeds without authorization: {len(rt_feeds_no_auth)}")
 
     # Turin
     url = 'http://percorsieorari.gtt.to.it/das_gtfsrt/vehicle_position.aspx'
-    vehicle_positions = get_vehicle_positions(url)
 
     feed = [f for f in rt_feeds_no_auth if f.url == url][0]
-    print(operator_name_to_topic(feed.operator_name))
-
-    for vehicle_position in vehicle_positions:
-        topic = f"stefan/gtfs-rt/{operator_name_to_topic(feed.operator_name)}/vehicle_positions/{vehicle_position.vehicle.id}"
-        print(f"Publishing to {topic}")
-
-    # Publish to MQTT:
-    # stefan/gtfs-rt/GTT_Servizio_Ferroviario/vehicle_positions/9060 -> Lat,Lon
-    # stefan/gtfs-rt/GTT_Servizio_Ferroviario/pb -> Protobuf message
-
-
-    # for feed in rt_feeds_no_auth:
-    #     print(feed)
-    #     try:
-    #         get_vehicle_positions(feed.url)
-    #     except Exception as e:
-    #         print("Error", feed, e)
+    fetch_from_feed(feed, mqtt_client)
 
 
 if __name__ == "__main__":
